f038384114d53525007c1bcad811e0c6765c74f4
Neloy Ahmed <web_one@pulseservicesbd.com>
2020-11-24 16:49:15 +0600

    work done commit

---
+++ app/Http/Controllers/Api/BookController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Book;
+
+class BookController extends Controller
+{
+    public function getAllBooks() {
+        $books = Book::get()->toJson(JSON_PRETTY_PRINT);
+        return response($books, 200);
+    }
+
+    public function getBook($id) {
+        if (Book::where('id', $id)->exists()) {
+          $book = Book::where('id', $id)->get()->toJson(JSON_PRETTY_PRINT);
+          return response($book, 200);
+        } else {
+          return response()->json([
+            "message" => "Book not found"
+          ], 404);
+        }
+    }
+
+    public function createBook(Request $request) {
+        $book = new Book;
+        $book->name = $request->name;
+        $book->author = $request->author;
+        $book->save();
+  
+        return response()->json([
+          "message" => "Book record created"
+        ], 201);
+    }
+
+    public function updateBook(Request $request, $id) {
+        if (Book::where('id', $id)->exists()) {
+          $book = Book::find($id);
+  
+          $book->name = is_null($request->name) ? $book->name : $book->name;
+          $book->author = is_null($request->author) ? $book->author : $book->author;
+          $book->save();
+  
+          return response()->json([
+            "message" => "records updated successfully"
+          ], 200);
+        } else {
+          return response()->json([
+            "message" => "Book not found"
+          ], 404);
+        }
+    }
+
+    public function deleteBook ($id) {
+        if(Book::where('id', $id)->exists()) {
+          $book = Book::find($id);
+          $book->delete();
+  
+          return response()->json([
+            "message" => "records deleted"
+          ], 202);
+        } else {
+          return response()->json([
+            "message" => "Book not found"
+          ], 404);
+        }
+      }
+}
